ba32945732ab23e4f30293e30473f6825660ef9a
downy <tkwk36@naver.com>
2024-05-10 11:49:32 +0900

    BOARD_TEST_JDW

--- board/src/main/java/com/aloha/board/controller/BoardController.java
+++ board/src/main/java/com/aloha/board/controller/BoardController.java
@@ -14,7 +14,6 @@ import com.aloha.board.server.BoardService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 
 
 
